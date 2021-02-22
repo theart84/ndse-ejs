e70c884ad6d35c0d03ddb0ce57fabb04f51ff75f
theart84 <artshadow74@gmail.com>
2021-02-22 12:22:08 +0500

    task completed

--- routes/books.js
+++ routes/books.js
@@ -3,12 +3,12 @@ const BooksController = require('../controllers/BooksController');
 const fileMiddleware = require('../middleware/file');
 const router = express.Router();
 
-router.get('/', BooksController.getBooks) // роут для получения всех книг
-router.get('/books/view/:id', BooksController.getBook); // роут для получения книги по id
-router.get('/books/create', BooksController.createBookGet); // роут для создания книги
-router.post('/books/create', fileMiddleware.single('filebook'), BooksController.createBookPost); // роут для создания книги post
-router.get('/books/update/:id', BooksController.updateBookGet); // роут для редактирования книги get
-router.post('/books/update/:id', fileMiddleware.single('filebook'), BooksController.updateBookPost); // роут для редактирования книги post
+router.get('/', BooksController.getBooks) // роут для главной страницы
+router.get('/books/view/:id', BooksController.getBook); // роут для просмотра книги по id
+router.get('/books/create', BooksController.createBookGet); // роут для получения страницы для создания книги
+router.post('/books/create', fileMiddleware.single('filebook'), BooksController.createBookPost); // роут для отправки запроса на создание книги
+router.get('/books/update/:id', BooksController.updateBookGet); // роут для получения страницы для редактирования книги
+router.post('/books/update/:id', fileMiddleware.single('filebook'), BooksController.updateBookPost); // роут для отправки запроса на редактирование книги
 router.post('/books/delete/:id', BooksController.deleteBook); // роут для удаления книги
 
 module.exports = router;
